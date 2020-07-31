1e4ee93426c7771eed0052c51af269ee9a42c286
manatsu525 <60680735+manatsu525@users.noreply.github.com>
2020-07-31 21:59:00 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,9 +20,9 @@ EOF
 wget https://github.com/manatsu525/net-speeder/releases/download/v1.0/net-speeder-master.zip
 unzip net-speeder-master.zip
 cd net-speeder-master/
-apt install libnet1-dev
-apt install libpcap0.8-dev
-apt install gcc
+apt install libnet1-dev -y
+apt install libpcap0.8-dev -y
+apt install gcc -y
 if hostnamectl status | grep -q openvz;then
     sh build.sh -DCOOKED
     service
